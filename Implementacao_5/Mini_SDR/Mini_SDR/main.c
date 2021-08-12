27040e894276303f2644e97e3318f9150bb13517
RODRIGO DE LIMA <49422747+rodrigols-10@users.noreply.github.com>
2021-08-12 19:56:32 -0300

    Adição do projeto ao repositório

---
+++ Implementacao_5/Mini_SDR/Mini_SDR/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 8/12/2021 7:54:20 PM
+ *  Author: rodri
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code
+    }
+}
