aa20bcb85c555c32e385380a1c3255f15ec2bc30
RODRIGO DE LIMA <49422747+rodrigols-10@users.noreply.github.com>
2021-08-12 21:04:19 -0300

    Primeiro teste com AM

--- Implementacao_5/Mini_SDR/Mini_SDR/main.c
+++ Implementacao_5/Mini_SDR/Mini_SDR/main.c
@@ -2,16 +2,18 @@
  * main.c
  *
  * Created: 8/12/2021 7:54:20 PM
- *  Author: rodri
+ *  Author: rodrigo
  */ 
 
 #include <xc.h>
+#include <avr/io.h>
 #include <math.h>
 
-float Ts = 0.000016;
+float Ts = 0.000216; //testar Ts = 0.000216 ---> como cada amostra demora 13 clocks do ADC, testar isso. prescale = 0.000016
 float t = 0;
 float pi = 3.14159;
 int fp = 100;
+float Tp;
 int		adsignal = 0;
 uint8_t	input=0;
 uint8_t	output = 0;
@@ -19,20 +21,20 @@ uint8_t offset = 0;
 
 int main(void)
 {
+	 Tp = 1/fp;
 	DDRD = 0b11111111;
 	DDRB = 0b11111111;
 
 	DDRC = 0b00000000;
 	ADMUX = 0b01000000;// Vcc como tensão de referência para os pinos analógicos, PC0 para conversão
-	ADCSRB = 0b00000000;
 	DIDR0 = 0b00111110;
-	ADCSRA = 0b10000111; //liga o conversor, prescale = 128
+	ADCSRB = 0b00000000;
+	ADCSRA = 0b11100111; //liga o conversor, habilita auto-trigger, prescale = 128
 	
     while(1)
     {
-        ADCSRA |= 1<<ADSC;    //Começa a conversão
+        //ADCSRA |= 1<<ADSC;    //Começa a conversão
         while(!(ADCSRA & 0b00010000)); //Espera o fim da conversão (Flag ADIF)
-        //adsignal = 511*cos(t)+511; //Simulação de uma entrada cosenoidal com amplitude 2.5V e offset 2.5V na resolução do Conversor AD.
         adsignal = ADC; // Sinal recebido pelo conversor AD do ATmega328P (Está em auto-trigger, ou seja, está constantemente convertendo e jogando o resultado em ADC)
         input = adsignal / 4;	   //de 10 bits para 8 bits.
 		
@@ -40,5 +42,7 @@ int main(void)
 		
 		PORTD = output;
 		t += Ts;
+		
+		if(t >= Tp) t = 0; //Quando a portadora completar 1 período, reiniciamos o t, para não haver contagem infinita.
     }
 }
