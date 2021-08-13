08e1e4413e61295701f21c93a87c6938d0b56cbf
RODRIGO DE LIMA <49422747+rodrigols-10@users.noreply.github.com>
2021-08-12 22:40:15 -0300

    AM funcionando, mas o tempo está errado.
    Frequência da portadora não confere na saída. O intervalo de amostragem usado deve estar errado.

--- Implementacao_5/Mini_SDR/Mini_SDR/main.c
+++ Implementacao_5/Mini_SDR/Mini_SDR/main.c
@@ -12,16 +12,17 @@
 float Ts = 0.000216; //testar Ts = 0.000216 ---> como cada amostra demora 13 clocks do ADC, testar isso. prescale = 0.000016
 float t = 0;
 float pi = 3.14159;
-int fp = 100;
-float Tp;
+int fp = 200;
+float Tp=0;
+float c=0;
 int		adsignal = 0;
 uint8_t	input=0;
 uint8_t	output = 0;
-uint8_t offset = 0;
+uint8_t offset = 127; //2.5V
 
 int main(void)
 {
-	 Tp = 1/fp;
+	Tp = 1/fp;
 	DDRD = 0b11111111;
 	DDRB = 0b11111111;
 
@@ -29,20 +30,23 @@ int main(void)
 	ADMUX = 0b01000000;// Vcc como tensão de referência para os pinos analógicos, PC0 para conversão
 	DIDR0 = 0b00111110;
 	ADCSRB = 0b00000000;
-	ADCSRA = 0b11100111; //liga o conversor, habilita auto-trigger, prescale = 128
-	
+	ADCSRA = 0b10000111; //liga o conversor, prescale = 128
+	ADCSRA |= 1<<ADSC;    //Começa a conversão
+	while(!(ADCSRA & 0b00010000)); //Espera o fim da conversão (Flag ADIF)
     while(1)
     {
-        //ADCSRA |= 1<<ADSC;    //Começa a conversão
-        while(!(ADCSRA & 0b00010000)); //Espera o fim da conversão (Flag ADIF)
+		ADCSRA |= 1<<ADSC;    //Começa a conversão
         adsignal = ADC; // Sinal recebido pelo conversor AD do ATmega328P (Está em auto-trigger, ou seja, está constantemente convertendo e jogando o resultado em ADC)
         input = adsignal / 4;	   //de 10 bits para 8 bits.
-		
-		output =(input - offset) * cos(2*pi*fp*t) + offset;
+		c = fp * t;
+		output = (input*0.9 - offset) * cos(2*pi*c) + offset;
 		
 		PORTD = output;
-		t += Ts;
 		
-		if(t >= Tp) t = 0; //Quando a portadora completar 1 período, reiniciamos o t, para não haver contagem infinita.
+		t += Ts;
+		if(c >= 1) t = 0; //Quando a portadora completar 1 período, reiniciamos o t, para não haver contagem infinita.
+		
+		while(!(ADCSRA & 0b00010000)); //Espera o fim da conversão (Flag ADIF)
+		ADCSRA &= 0b11101111;
     }
 }
