6eb8cd936a3358b5a8ca513786e79886d2513d84
RODRIGO DE LIMA <49422747+rodrigols-10@users.noreply.github.com>
2021-08-14 22:40:27 -0300

    Modulacao feita

--- Implementacao_5/Mini_SDR/Mini_SDR/main.c
+++ Implementacao_5/Mini_SDR/Mini_SDR/main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  * Created: 8/12/2021 7:54:20 PM
- *  Author: rodrigo
+ *  Author: rodrigo & Marcelo & Joao Matheus Bernardo Resende & Wesley Brito & Anny Pinheiro
  */ 
 
 //#define F_CPU 16000000UL	//freq de trabalho da CPU
@@ -29,7 +29,7 @@ uint8_t Ap = 127;			//Amplitude da portadora em 2.5V
 
 //VARIAVEIS DE AJUSTES
 uint16_t adc_value = 0;
-int AM=0, FM=0, ASK=0, FSK=0;
+int mod=1; //Inicia em AM;
 
  //bool button;
 
@@ -40,6 +40,16 @@ void adc_initiate();
 uint16_t freq_limit (uint16_t adc_value);
 void mod_select (uint16_t adc_value);
 
+void delay_1();
+void lcd_cmd(unsigned char cmd);
+void lcd_data(unsigned char data);
+void lcd_adress(unsigned char adress);
+void lcd_init();
+void lcd_default();
+void lcd_mod(int mod);
+void lcd_off_cursor();
+void lcd_on_cursor();
+
 ISR(ADC_vect){
 	adc_value = ADC;
 }
@@ -47,7 +57,7 @@ ISR(ADC_vect){
 int main(void)
 {
 	DDRD = 0b11111111;
-	DDRB = 0b11111111;
+	DDRB = 0b00111111;
 
 	DDRC = 0b00000000;
 	PORTC = 0b00000000;
@@ -57,8 +67,11 @@ int main(void)
 	adc_initiate();		
 	sei();			// HABILITA INTERRUPCAO GLOBAL
 
-	//VALORES DEFAULT
-	AM=1;
+	lcd_init();								//Init LCD
+	lcd_off_cursor();
+	//lcd_on_cursor();
+	lcd_default();
+	
 
     while(1)
     {
@@ -70,19 +83,19 @@ int main(void)
 		//       MODULAÇÃO AM
 		// -----------------------------	
 		
-		if(AM == 1)	output = ((input/2) * cos(2*pi*fp*t)) + offset;
+		if(mod == 1)	output = ((input/2) * cos(2*pi*fp*t)) + offset;
 		
 		// -----------------------------
 		//       MODULAÇÃO FM
 		// -----------------------------
 		
-		if(FM == 1);
+		if(mod == 2);
 		
 		// -----------------------------
 		//       MODULAÇÃO ASK
 		// -----------------------------
 		
-		if(ASK == 1){
+		if(mod == 3){
 			if(input < 10) output = offset; //Significa que a entrada está em zero. (caso o valor de zero fique com ruído)
 			else {
 			output = (input/2) * cos(2*pi*fp*t) + offset;
@@ -93,7 +106,7 @@ int main(void)
 		//       MODULAÇÃO FSK
 		// -----------------------------
 		
-		if(FSK == 1){
+		if(mod == 4){
 			if(input < 10) output = Ap * cos(2*pi*fp*t) + offset; //Significa que a entrada está em zero. (caso o valor de zero fique com ruído)
 			else {
 			output = Ap * cos(4*pi*fp*t) + offset; //Dobra a frequência da portadora
@@ -115,9 +128,16 @@ int main(void)
 		// -----------------------------		
 		//button = ;
 		if(PINC & (1<<PINC2)){
+//		<<< pre-configuracao de modulacao aqui >>>
+			ADMUX |= 0x01;
 			while (!(PINC & (1<<PINC3)))
 			{
 				modulacao();
+			}
+//		<<< pre-configuracao de portadora aqui >>>
+			while (!(PINC & (1<<PINC4)))
+			{
+				portadora();
 			}
 			ADMUX &= 0xFE;
 		}
@@ -127,12 +147,7 @@ int main(void)
 
 void modulacao()
 {
-	//<<<< ou aqui >>>>
-	ADMUX |= 0x01;
-	input = adc_value / 4;	   //de 10 bits para 8 bits.
-	output = input;
-	PORTD = output;
-	//<<<<<<< codigo do display aqui >>>>>>>
+	mod_select (adc_value);
 }
 
 void portadora()
@@ -178,27 +193,345 @@ uint16_t freq_limit (uint16_t adc_value){
 //Funcao seletora do tipo de modulacao usando o potenciometro como parametro.
 void mod_select (uint16_t adc_value){
 	if(adc_value >= 0 && adc_value <= 255){
-		AM = 1;
-		FM = 0;
-		ASK = 0;
-		FSK = 0;
+		mod = 1;
+		lcd_mod(1);
 	}
 	else if(adc_value >= 256 && adc_value <= 510){
-		AM = 0;
-		FM = 1;
-		ASK = 0;
-		FSK = 0;
+		mod = 2;
+		lcd_mod(2);
 	}
 	else if(adc_value >= 511 && adc_value <= 765){
-		AM = 0;
-		FM = 0;
-		ASK = 1;
-		FSK = 0;
+		mod = 3;
+		lcd_mod(3);
 	}
 	else if(adc_value >= 766 && adc_value <= 1023){
-		AM = 0;
-		FM = 0;
-		ASK = 0;
-		FSK = 1;
+		mod = 4;
+		lcd_mod(4);
+	}
+}
+
+//----------------------------------------------------------
+//						DISPLAY LCD
+//----------------------------------------------------------
+
+void delay_1(){
+	for(int i = 0;i<200;i++){}
+}
+
+void lcd_cmd(unsigned char cmd){
+	
+	PORTB &= 0xF0;								// Mask preservation 4 LSBs
+	PORTB |= cmd;								// Add data command
+	
+	PORTB &= ~(1<<PORTB5);						// Set RS = 0
+	PORTB |= (1<<PORTB4);						// Set E = 1
+	delay_1();
+	PORTB &= ~(1<<PORTB4);						// Set E = 0
+
+}
+
+void lcd_data(unsigned char data){
+	
+	PORTB |= (1<<PORTB5);						// Set RS = 1
+	
+	for(int i=0; i<2 ;i++){						// Send two data (2x 4 bits)
+		if(i==0){
+			
+			char MSB_data = data >> 4;			// Shift right 4 Bits MSB >> LSB
+			PORTB &= 0xF0;						// Mask preservation 4 MSBs
+			PORTB |= MSB_data;					// Add data
+			
+			PORTB |= (1<<PORTB4);				// Set E = 1
+			delay_1();
+			PORTB &= ~(1<<PORTB4);				// Set E = 0
+		}
+		
+		else{
+			PORTB &= 0xF0;						// Mask preservation 4 LSBs
+			PORTB |= data;						// Add data
+			
+			PORTB |= (1<<PORTB4);				// Set E = 1
+			delay_1();
+			PORTB &= ~(1<<PORTB4);				// Set E = 0
+		}
+	}
+	PORTB &= ~(1<<PORTB5);						// Set RS = 0
+
+}
+void lcd_adress(unsigned char adress){
+	
+	PORTB &= ~(1<<PORTB5);						// Set RS = 0
+	
+	for(int i=0; i<2 ;i++){						// Send two data (2x 4 bits)
+		if(i==0){
+			
+			char MSB_adress = adress >> 4;		// Shift right 4 Bits MSB >> LSB
+			PORTB &= 0xF0;						// Mask preservation 4 MSBs
+			PORTB |= MSB_adress;				// Add data
+			
+			PORTB |= (1<<PORTB4);				// Set E = 1
+			delay_1();
+			PORTB &= ~(1<<PORTB4);				// Set E = 0
+		}
+		
+		else{
+			PORTB &= 0xF0;						// Mask preservation 4 LSBs
+			PORTB |= adress;						// Add data
+			
+			PORTB |= (1<<PORTB4);				// Set E = 1
+			delay_1();
+			PORTB &= ~(1<<PORTB4);				// Set E = 0
+		}
+	}
+	PORTB &= ~(1<<PORTB5);					// Set RS = 0
+}
+void lcd_init(){
+	
+	lcd_cmd(0x02);							//RS = 0 D7:D4 = 0010
+	lcd_cmd(0x02);							//RS = 0 D7:D4 = 0010
+	lcd_cmd(0x08);							//RS = 0 D7:D4 = 1000
+	
+}
+
+void lcd_off_cursor(){
+
+	lcd_cmd(0x00);							//RS = 0 D7:D4 = 0000
+	lcd_cmd(0x0C);							//RS = 0 D7:D4 = 1100
+}
+
+void lcd_on_cursor(){
+	
+	lcd_cmd(0x00);							//RS = 0 D7:D4 = 0000
+	lcd_cmd(0x0F);							//RS = 0 D7:D4 = 1100
+}
+
+void lcd_default(){
+	
+	lcd_adress(0x80);
+	lcd_data(0x4D);							//M
+	lcd_adress(0x81);
+	lcd_data(0x6F);							//o
+	lcd_adress(0x82);
+	lcd_data(0x64);							//d
+	lcd_adress(0x83);
+	lcd_data(0x3A);							//:
+	
+	lcd_adress(0xC0);
+	lcd_data(0x4D);							//M
+	lcd_adress(0xC1);
+	lcd_data(0x73);							//s
+	lcd_adress(0xC2);
+	lcd_data(0x67);							//g
+	lcd_adress(0xC3);
+	lcd_data(0x3A);							//:
+}
+
+void lcd_mod(int mod){
+	switch(mod){
+		case 1:								//AM
+		lcd_adress(0x85);
+		lcd_data(0x41);
+		
+		lcd_adress(0x86);
+		lcd_data(0x4D);
+		
+		lcd_adress(0x87);
+		lcd_data(0x01);
+		
+		lcd_adress(0x89);
+		lcd_data(0x46);					//F
+		lcd_adress(0x8A);
+		lcd_data(0x3A);					//:
+		
+		lcd_adress(0X8D);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8C);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8B);
+		lcd_data(0xB0);					//-
+		
+		lcd_adress(0x8E);
+		lcd_data(0x48);					//H
+		lcd_adress(0x8F);
+		lcd_data(0x7A);					//z
+		
+		lcd_adress(0XC5);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC6);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC7);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC8);
+		lcd_data(0x01);					//
+		lcd_adress(0XC9);
+		lcd_data(0x01);					//
+		lcd_adress(0XCA);
+		lcd_data(0x01);					//
+		lcd_adress(0XCB);
+		lcd_data(0x01);					//
+		lcd_adress(0XCC);
+		lcd_data(0x01);					//
+		lcd_adress(0XCD);
+		lcd_data(0x01);					//
+		lcd_adress(0XCE);
+		lcd_data(0x01);					//
+		lcd_adress(0XCF);
+		lcd_data(0x01);					//
+		
+		break;
+		
+		case 2:								//FM
+		lcd_adress(0x85);
+		lcd_data(0x46);
+		
+		lcd_adress(0x86);
+		lcd_data(0x4D);
+		
+		lcd_adress(0x87);
+		lcd_data(0x01);
+		lcd_adress(0x89);
+		
+		lcd_adress(0x89);
+		lcd_data(0x46);					//F
+		lcd_adress(0x8A);
+		lcd_data(0x3A);					//:
+		
+		lcd_adress(0X8D);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8C);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8B);
+		lcd_data(0xB0);					//-
+		
+		lcd_adress(0x8E);
+		lcd_data(0x48);					//H
+		lcd_adress(0x8F);
+		lcd_data(0x7A);					//z
+		
+		lcd_adress(0XC5);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC6);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC7);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC8);
+		lcd_data(0x01);					//
+		lcd_adress(0XC9);
+		lcd_data(0x01);					//
+		lcd_adress(0XCA);
+		lcd_data(0x01);					//
+		lcd_adress(0XCB);
+		lcd_data(0x01);					//
+		lcd_adress(0XCC);
+		lcd_data(0x01);					//
+		lcd_adress(0XCD);
+		lcd_data(0x01);					//
+		lcd_adress(0XCE);
+		lcd_data(0x01);					//
+		lcd_adress(0XCF);
+		lcd_data(0x01);					//
+		break;
+		
+		case 3:								//ASK
+		lcd_adress(0x85);
+		lcd_data(0x41);
+		
+		lcd_adress(0x86);
+		lcd_data(0x53);
+		
+		lcd_adress(0x87);
+		lcd_data(0x4B);
+		
+		lcd_adress(0x89);
+		lcd_data(0x54);					//T
+		lcd_adress(0x8A);
+		lcd_data(0x3A);					//:
+		
+		lcd_adress(0X8D);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8C);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8B);
+		lcd_data(0xB0);					//-
+		
+		lcd_adress(0x8E);
+		lcd_data(0x62);					//b
+		lcd_adress(0x8F);
+		lcd_data(0x73);					//s
+		
+		lcd_adress(0XC5);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC6);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC7);
+		lcd_data(0x2E);					//.
+		lcd_adress(0XC8);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC9);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCA);
+		lcd_data(0x2E);					//.
+		lcd_adress(0XCB);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCC);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCD);
+		lcd_data(0x2E);					//.
+		lcd_adress(0XCE);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCF);
+		lcd_data(0xB0);					//-
+		break;
+		
+		case 4:								//FSK
+		lcd_adress(0x85);
+		lcd_data(0x46);
+		
+		lcd_adress(0x86);
+		lcd_data(0x53);
+		
+		lcd_adress(0x87);
+		lcd_data(0x4B);
+		
+		lcd_adress(0x89);
+		lcd_data(0x54);					//T
+		lcd_adress(0x8A);
+		lcd_data(0x3A);					//:
+		
+		lcd_adress(0X8D);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8C);
+		lcd_data(0xB0);					//-
+		lcd_adress(0X8B);
+		lcd_data(0xB0);					//-
+		
+		lcd_adress(0x8E);
+		lcd_data(0x62);					//b
+		lcd_adress(0x8F);
+		lcd_data(0x73);					//s
+		
+		lcd_adress(0XC5);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC6);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC7);
+		lcd_data(0x2E);					//.
+		lcd_adress(0XC8);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XC9);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCA);
+		lcd_data(0x2E);					//.
+		lcd_adress(0XCB);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCC);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCD);
+		lcd_data(0x2E);					//.
+		lcd_adress(0XCE);
+		lcd_data(0xB0);					//-
+		lcd_adress(0XCF);
+		lcd_data(0xB0);					//-
+		break;
 	}
 }
